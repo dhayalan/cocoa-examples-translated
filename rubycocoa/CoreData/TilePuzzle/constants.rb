8ac256590299ab90d7808868ea1389a31b4ee04e
Brian Marick <marick@exampler.com>
2009-04-24 15:06:36 -0500

    Delete old versions of TilePuzzle

--- rubycocoa/CoreData/TilePuzzle/constants.rb
+++
@@ -1,5 +0,0 @@
-TileGridSize=4 
-NumTiles=TileGridSize * TileGridSize
-PuzzleImageName = "TigerPuzzle.png"
-
-NSXMLStoreType = "XML"  # This is value of a C global, so not directly accessible to RubyCocoa.
