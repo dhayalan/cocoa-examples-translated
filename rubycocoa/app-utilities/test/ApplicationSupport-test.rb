4bb00c5d0a7e1a617fb0bb27b9917ef99506b63d
Brian Marick <marick@exampler.com>
2009-05-13 15:49:56 -0500

    tested ApplicationSupport object into existence

--- rubycocoa/app-utilities/test/ApplicationSupport-test.rb
+++ rubycocoa/app-utilities/test/ApplicationSupport-test.rb
@@ -6,9 +6,66 @@ class ApplicationSupportTest < Test::Unit::TestCase
   
   context "initialization" do
 
-    should "blah" do
-      ApplicationSupport.alloc.initForApp("ThisOne")
-      assert { false }
+    setup do 
+      @file_manager = rubycocoa_flexmock("file manager")
+      @low_level_fileops = rubycocoa_flexmock("low level file operations")
+      @test_options = {
+        :file_manager => @file_manager,
+        :low_level_fileops => @low_level_fileops
+      }
+
+      @low_level_fileops.should_receive('NSSearchPathForDirectoriesInDomains', 3).
+                         and_return(['/userhome'].to_ns).
+                         by_default
+      @file_manager.should_receive(:fileExistsAtPath, 1).
+                    and_return(true).
+                    by_default
+    end
+
+    should "ask Cocoa to give the user domain's application support" do
+      during { 
+        ApplicationSupport.new("irrelevant", @test_options).pathname
+      }.behold! {
+        @low_level_fileops.should_receive('NSSearchPathForDirectoriesInDomains', 3).once.
+                           with(NSApplicationSupportDirectory, NSUserDomainMask, any).
+                           and_return(['/userhome'].to_ns)
+      }
+    end
+
+
+    should "return a result without tildes" do
+      during { 
+        ApplicationSupport.new("irrelevant", @test_options).pathname
+      }.behold! {
+        @low_level_fileops.should_receive('NSSearchPathForDirectoriesInDomains', 3).once.
+                           with(any, any, true).
+                           and_return(['/userhome'].to_ns)
+      }
+    end
+
+    should "add the application name to the the application support directory" do
+      as = ApplicationSupport.new("AppName", @test_options)
+      assert { as.pathname == "/userhome/AppName" }
+    end
+
+    should "also be able to return a URL." do
+      as = ApplicationSupport.new("AppName", @test_options)
+      # Equality is pointer-equality for NSURLs.
+      assert { as.url.absoluteString ==
+               NSURL.fileURLWithPath("/userhome/AppName").absoluteString }
+    end
+
+    should "create the application's directory if needed" do
+      during { 
+        ApplicationSupport.new("AppName", @test_options).pathname
+      }.behold! {
+        @file_manager.should_receive(:fileExistsAtPath, 1).once.
+                      with("/userhome/AppName").
+                      and_return(false)
+        @file_manager.should_receive(:createDirectoryAtPath_attributes, 2).once.
+                      with("/userhome/AppName", nil)
+                  
+      }
     end
   end
 end
